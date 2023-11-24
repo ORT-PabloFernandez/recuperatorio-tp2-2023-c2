21a312da4acec32c1cb8d1aa6f087648171ea8a2
eileenss <eileenssanchez97@gmail.com>
2023-11-23 21:56:03 -0300

    Eileen Sanchez

--- controllers/sales.js
+++ controllers/sales.js
@@ -4,4 +4,28 @@ async function getSales(){
     return sales.getAllSales();
 }
 
-module.exports = {getSales};
+//1. Venta particulas por _id
+async function getSale(id){
+    return sales.getSale(id);
+}
+
+//2. Ventas filtradas por metodo de compra "purchaseMethod"
+async function getSalesByPurchaseMethod(purchaseMethod){
+    return sales.getSalesByPurchaseMethod(purchaseMethod);
+}
+
+//3. Ventas de un cliente por email
+async function getSalesCustomerByEmail(email){
+    return sales.getSalesCustomerByEmail(email);
+}
+
+//4. Clientes por satisfaccion
+async function getSalesCustomersSatisfaction(num){
+    return sales.getSalesCustomersSatisfaction(num);
+}
+
+async function getSalesByLocation(location){
+    return sales.getSalesByLocation(location);
+}
+
+module.exports = {getSales, getSale, getSalesByPurchaseMethod, getSalesCustomerByEmail, getSalesCustomersSatisfaction, getSalesByLocation};
